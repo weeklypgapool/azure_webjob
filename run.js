68db904551d386d1a0307b612590dbe5807e4373
Jim Clark <jim@managerincharge.com>
2014-08-06 04:19:32 -0700

    changed fb data structure

--- run.js
+++ run.js
@@ -4,7 +4,7 @@ var http = require('http');
 var path = require('path');
 var _ = require('lodash');
 var Firebase = require('firebase');
-var MongoClient = require('mongodb').MongoClient;
+//var MongoClient = require('mongodb').MongoClient;
 
 var curTourneyRef = new Firebase('https://weeklypgapool.firebaseio.com/tournaments/current'),
 		dataUrl, last_updated,
@@ -17,7 +17,20 @@ function ExitNode() {
 }
 
 function PutPgaJsonIntoFb(pgaJson) {
-	curTourneyRef.child('data').set(pgaJson, ExitNode());
+	var lb = {
+		"leaderboard": pgaJson.leaderboard
+	};
+	delete pgaJson.leaderboard;
+	curTourneyRef.child('data').set(lb, function () {
+		curTourneyRef.child('data/stats').set(pgaJson, function (err) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log('success');
+				ExitNode();
+			}
+		});
+	});
 }
 
 function FormatPgaJson(pgaJson) {
@@ -48,7 +61,7 @@ function FormatPgaJson(pgaJson) {
 	return json;
 }
 
-function GetPgaJson(dataUrl) {
+function ExtractPgaDataIntoFb(dataUrl) {
 	http.get(dataUrl, function (resp) {
 		pgaJson = "";
 		resp.on('data', function (chunk) {
@@ -56,56 +69,44 @@ function GetPgaJson(dataUrl) {
 		});
 		resp.on('end', function () {
 			pgaJson = JSON.parse(pgaJson);
-			if (pgaJson.last_updated === last_updated) { process.exit(); };
+			if (pgaJson.last_updated === last_updated) { ExitNode(); }
 			pgaJson = FormatPgaJson(pgaJson);
 			PutPgaJsonIntoFb(pgaJson);
 		});
 	}).end();
 }
 
-// ----  Main Processing
-
-// Exit if not Thu thru Sun, or too early, or too late
-var now = new Date();
-var dow = now.getDay();
-// Ensure that it's Thu thru Sun
-if (dow === 0 || (dow >= 4 && dow <= 6)) {
+function IsWithinWindow() {
+	// Exit if not Thu thru Sun, or too early, or too late
+	var now = new Date();
+	var dow = now.getDay();
+	// Ensure that it's Thu thru Sun
+	if (dow !== 0 && (dow < 4 || dow > 6)) { return false; }
 	// Ensure that time is reasonable - between 4 am PST and 8 pm PST
 	var time = dow.getTime();
-	if (time >= 1407236400000 && time <= 1407294000000) {
-		// Get the website url for the current tournament.  This value is currently
-		// manually entered into the fb.  Data transfer is done inside GetPgaJson()
-		curTourneyRef.child('dataUrl').once('value', function (snap) {
-			dataUrl = snap.val();
-			curTourneyRef.child('data/last_updated').once('value', function (snap) {
-				last_updated = snap.val();
-				pgaJson = GetPgaJson(dataUrl);
-			});
+	if (time < 1407236400000 || time > 1407294000000) { return false; }
+	return true;
+}
+
+// ----  Main Processing
+
+if (IsWithinWindow()) {
+	// Get the website url for the current tournament.  This value is currently
+	// manually entered into the fb.  Data transfer is done inside GetPgaJson()
+	curTourneyRef.child('dataUrl').once('value', function (snap) {
+		dataUrl = snap.val();
+		curTourneyRef.child('data/last_updated').once('value', function (snap) {
+			last_updated = snap.val();
+			ExtractPgaDataIntoFb(dataUrl);
 		});
-	} else {
-		console.log('Not between 4 am and 8 pm');
-		process.exit();
-	}
+	});
 } else {
-	console.log('Not Thu thru Sun');
-	process.exit();
-};
+	console.log('Not within time window');
+	ExitNode();
+}
 
 
-
-
-//if (today is monday thru wed) quit
-//if (time is > 6 pm) quit
-//if (time is < 5 am) quit
-//download from pga site
-//if (is_started) {
-//	//tourney has started, continue
-//	if ()
-//	
-//}
-//
-
-//// Load last update info stored in Mongodb
+// Load last update info stored in Mongodb
 //MongoClient.connect('mongodb://weeklypgapool:wpgap0333@ds050087.mongolab.com:50087/weeklypgapool', function(err, db) {
 //	if (err) { process.exit(1); }
 //	var coll = db.collection('last_update_cache');
