ee3d1ec450c84c4de081cb0cd020d96878e84e0c
jim-clark <email@jim-clark.com>
2015-07-19 10:14:53 -0700

    Implemented custom start and end windows

--- run.js
+++ run.js
@@ -7,12 +7,13 @@ var _ = require('lodash');
 var Firebase = require('firebase');
 
 var curTourneyRef = new Firebase('https://weeklypgapool.firebaseio.com/tournaments/current'),
-		dataUrl,
-		last_updated,
-		pgaJson,
-		round_state,
-		isGolfDotCom = false,
-		money_computed_manually;
+	dataUrl,
+	last_updated,
+	pgaJson,
+	round_state,
+	isGolfDotCom = false,
+	money_computed_manually,
+	windowStart, windowEnd;
 
 // Authenticate to fb
 curTourneyRef.auth('uOdRH5Zyzy4QzGSB1HFO2thq6KsKrTWx3FTSKd8A');
@@ -132,10 +133,6 @@ function ComputeMoneyAndPutInJsonGolfDotCom(json, callback) {
 	} else {
 		totalPurse = json.money.total_purse;
 		moneyByPos = BuildMoneyArray(totalPurse);
-		
-		
-		
-		
 		ProcessPayoutsGolfDotCom(json, moneyByPos);
 		callback();
 	}
@@ -318,8 +315,12 @@ function IsWithinWindow(callback) {
 			// Exit if not Thu thru Sun, or too early, or too late
 			var now = new Date();
 			var dow = now.getDay();
-			// Ensure that it's Thu thru Sun
-			if (dow !== 0 && (dow < 4 || dow > 7)) {
+			// Check for custom start/end window
+			if (windowStart) {
+				if (now < Date(windowStart) || now > Date(windowEnd)) {
+					ExitNode();
+				}
+			} else if (dow !== 0 && (dow < 4 || dow > 7)) {   // Ensure that it's Thu thru Sun
 				console.log('Not within time window');
 				ExitNode();
 			}
@@ -344,8 +345,11 @@ function DoWork() {
 			last_updated = snap.val();
 			curTourneyRef.child('data/stats/round_state').once('value', function (snap) {
 				round_state = snap.val();
-				curTourneyRef.child('money_computed_manually').once('value', function (snap) {
-					money_computed_manually = snap.val();
+				curTourneyRef.child('custom_config').once('value', function (snap) {
+					var config = snap.val();
+					money_computed_manually = config.money_computed_manually;
+					windowStart = config.window_start;
+					windowEnd = config.window_end;
 					ExtractPgaDataIntoFb(dataUrl);
 				});
 			});
